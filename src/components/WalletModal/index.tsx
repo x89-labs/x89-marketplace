398d4e48a278eac5a9fac9ff68579d46e4b4b9f5
stevenlee2806 <steven.lee2806@gmail.com>
2021-06-18 02:15:50 +0700

    update docker

--- src/components/WalletModal/index.tsx
+++ src/components/WalletModal/index.tsx
@@ -346,7 +346,7 @@ export default function WalletModal({
             <AutoRow style={{ flexWrap: 'nowrap' }}>
               <TYPE.main fontSize={14}>
                 <Trans>
-                  By connecting a wallet, you agree to Uniswap Labs’{' '}
+                  By connecting a wallet, you agree to X89 Labs’{' '}
                   <ExternalLink href="https://uniswap.org/terms-of-service/">Terms of Service</ExternalLink> and
                   acknowledge that you have read and understand the{' '}
                   <ExternalLink href="https://uniswap.org/disclaimer/">Uniswap protocol disclaimer</ExternalLink>.
