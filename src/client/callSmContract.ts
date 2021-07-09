e5a7093c55d902c37476cc390b1daab5fb179d75
stevenlee2806 <steven.lee2806@gmail.com>
2021-07-09 16:10:53 +0700

    update package

--- src/client/callSmContract.ts
+++ src/client/callSmContract.ts
@@ -1,6 +1,6 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 import { ethers } from 'ethers'
-import Web3 from 'web3'
+// import Web3 from 'web3'
 
 const contract = require('../abis/XNFT.json')
 export const contractAddress = '0x398bcBcd555326A875C6e6A52907807303E6Af15'
@@ -11,31 +11,29 @@ const NODE_URL = 'https://ropsten.infura.io/v3/5e4235dd371d43f0bbc8d252f58ac94c'
 
 export async function mintNFT(tokenURI?: string) {
   if (NODE_URL) {
-    const web3 = new Web3(new Web3.providers.HttpProvider(NODE_URL))
-    const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
-    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest') //get latest nonce
-
-    const tx = {
-      from: PUBLIC_KEY,
-      to: contractAddress,
-      gasLimit: 500000,
-      nonce: nonce,
-      data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
-    }
-    const signPromise = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
-    console.log(signPromise)
-
-    signPromise.rawTransaction &&
-      web3.eth.sendSignedTransaction(signPromise.rawTransaction, function (err, hash) {
-        if (!err) {
-          console.log(
-            'The hash of your transaction is: ',
-            hash,
-            "\nCheck Alchemy's Mempool to view the status of your transaction!"
-          )
-        } else {
-          console.log('Something went wrong when submitting your transaction:', err)
-        }
-      })
+    // const web3 = new Web3(new Web3.providers.HttpProvider(NODE_URL))
+    // const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
+    // const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest') //get latest nonce
+    // const tx = {
+    //   from: PUBLIC_KEY,
+    //   to: contractAddress,
+    //   gasLimit: 500000,
+    //   nonce: nonce,
+    //   data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
+    // }
+    // const signPromise = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
+    // console.log(signPromise)
+    // signPromise.rawTransaction &&
+    //   web3.eth.sendSignedTransaction(signPromise.rawTransaction, function (err, hash) {
+    //     if (!err) {
+    //       console.log(
+    //         'The hash of your transaction is: ',
+    //         hash,
+    //         "\nCheck Alchemy's Mempool to view the status of your transaction!"
+    //       )
+    //     } else {
+    //       console.log('Something went wrong when submitting your transaction:', err)
+    //     }
+    //   })
   }
 }
